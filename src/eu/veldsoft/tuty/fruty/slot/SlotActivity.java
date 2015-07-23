2bb8349b4942ee30d3c4f19f25558f88b5a172b0
Todor Balabanov <todor.balabanov@gmail.com>
2015-07-23 13:48:18 +0300

    Indicators digits were added.

--- src/eu/veldsoft/tuty/fruty/slot/SlotActivity.java
+++ src/eu/veldsoft/tuty/fruty/slot/SlotActivity.java
@@ -2,10 +2,12 @@ package eu.veldsoft.tuty.fruty.slot;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class SlotActivity extends Activity {
 
@@ -13,6 +15,16 @@ public class SlotActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_slot);
+
+		Typeface typeface = Typeface.createFromAsset(getAssets(),
+				getResources().getString(R.string.digits_font_path));
+		((TextView) findViewById(R.id.single_bet_value)).setTypeface(typeface);
+		((TextView) findViewById(R.id.active_lines_value))
+				.setTypeface(typeface);
+		((TextView) findViewById(R.id.total_bet_value)).setTypeface(typeface);
+		((TextView) findViewById(R.id.winner_paied_value))
+				.setTypeface(typeface);
+		((TextView) findViewById(R.id.credit_value)).setTypeface(typeface);
 	}
 
 	@Override
